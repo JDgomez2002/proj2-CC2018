8076b2c2291a84d5ba3d7a95870ddb0297645928
JDgomez2002 <gom21429@uvg.edu.gt>
2023-11-23 11:49:24 -0600

    The Last of Us

---
+++ src/main.cpp
@@ -0,0 +1,303 @@
+#include <SDL2/SDL.h>
+#include <SDL_events.h>
+#include <SDL_render.h>
+#include <SDL_video.h>
+#include <SDL2/SDL_ttf.h>  // Include the SDL_ttf header
+#include <SDL2/SDL_mixer.h>
+#include "color.h"
+#include "imageloader.h"
+#include "raycaster.h"
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+TTF_Font* font;  // Declare the font variable
+
+void clear() {
+    // Define the colors at the top and bottom of the screen
+    Color topColor = {24, 27, 34, 255};  // color of the top wall
+    Color bottomColor = {24, 27, 34, 255};  // Midnight blue
+
+    // Draw each row of the screen
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        // Calculate the ratio of the current row to the screen height
+        float ratio = static_cast<float>(y) / SCREEN_HEIGHT;
+
+        // Calculate the color for this row by linearly interpolating between the top and bottom colors
+        Color rowColor;
+        rowColor.r = static_cast<Uint8>(topColor.r * (1 - ratio) + bottomColor.r * ratio);
+        rowColor.g = static_cast<Uint8>(topColor.g * (1 - ratio) + bottomColor.g * ratio);
+        rowColor.b = static_cast<Uint8>(topColor.b * (1 - ratio) + bottomColor.b * ratio);
+        rowColor.a = 255;
+
+        // Set the draw color to the color for this row
+        SDL_SetRenderDrawColor(renderer, rowColor.r, rowColor.g, rowColor.b, rowColor.a);
+
+        // Draw the row
+        SDL_Rect row;
+        row.x = 0;
+        row.y = y;
+        row.w = SCREEN_WIDTH;
+        row.h = 1;
+        SDL_RenderFillRect(renderer, &row);
+    }
+}
+
+void draw_floor(int value) {
+    // floor color
+    if (value == 0){
+        SDL_SetRenderDrawColor(renderer, 24, 27, 34, 255);
+    } else {
+        SDL_SetRenderDrawColor(renderer, 24, 27, 34, 255);
+    }
+    SDL_Rect rect = {
+            0,  // Coordenada x modificada para comenzar desde el lado izquierdo
+            SCREEN_HEIGHT / 2,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT / 2
+    };
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+// Variables globales para almacenar la posición anterior del mouse
+int previousMouseX = 0;
+
+// Función para manejar los eventos del mouse
+Raycaster handleMouseEvents(SDL_Event event, Raycaster r) {
+    if (event.type == SDL_MOUSEMOTION) {
+        if (event.motion.state & SDL_BUTTON_LMASK) {
+            // Calcular el cambio en la posición del mouse
+            int deltaX = event.motion.x - previousMouseX;
+
+            // Ajustar la rotación del jugador según el movimiento del mouse
+            float rotationAmount = static_cast<float>(deltaX) * (3.14 / 24);
+            r.player.a -= rotationAmount;
+
+            // Actualizar la posición anterior del mouse
+            previousMouseX = event.motion.x;
+        }
+    }
+    return r;
+}
+
+// Function to display the welcome message
+void displayWelcomeMessage() {
+    SDL_SetRenderDrawColor(renderer, 56, 56, 56, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Color textColor = { 255, 255, 255, 255 };
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "Welcome The Last of Us, find the window and press right click to escape", textColor);
+    SDL_Texture* welcomeMessage = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect messageRect;
+    messageRect.x = (SCREEN_WIDTH - surfaceMessage->w) / 2;
+    messageRect.y = (SCREEN_HEIGHT - surfaceMessage->h) / 2;
+    messageRect.w = surfaceMessage->w;
+    messageRect.h = surfaceMessage->h;
+
+    SDL_RenderCopy(renderer, welcomeMessage, nullptr, &messageRect);
+    SDL_RenderPresent(renderer);
+
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(welcomeMessage);
+}
+
+void displayWinningMessage() {
+    SDL_SetRenderDrawColor(renderer, 56, 56, 56, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Color textColor = { 255, 255, 255, 255 };
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "You escaped!", textColor);
+    SDL_Texture* welcomeMessage = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect messageRect;
+    messageRect.x = (SCREEN_WIDTH - surfaceMessage->w) / 2;
+    messageRect.y = (SCREEN_HEIGHT - surfaceMessage->h) / 2;
+    messageRect.w = surfaceMessage->w;
+    messageRect.h = surfaceMessage->h;
+
+    SDL_RenderCopy(renderer, welcomeMessage, nullptr, &messageRect);
+    SDL_RenderPresent(renderer);
+
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(welcomeMessage);
+}
+
+void displayLosingMessage() {
+    SDL_SetRenderDrawColor(renderer, 56, 56, 56, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_Color textColor = { 255, 255, 255, 255 };
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "Error, please try again", textColor);
+    SDL_Texture* welcomeMessage = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    SDL_Rect messageRect;
+    messageRect.x = (SCREEN_WIDTH - surfaceMessage->w) / 2;
+    messageRect.y = (SCREEN_HEIGHT - surfaceMessage->h) / 2;
+    messageRect.w = surfaceMessage->w;
+    messageRect.h = surfaceMessage->h;
+
+    SDL_RenderCopy(renderer, welcomeMessage, nullptr, &messageRect);
+    SDL_RenderPresent(renderer);
+
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(welcomeMessage);
+}
+
+void drawUI(int value) {
+    int size = 720;
+    if (value == 0){
+        ImageLoader::render(renderer, "p", SCREEN_WIDTH/2.0f - size/2.0f, SCREEN_HEIGHT - size, size);
+    } else {
+        ImageLoader::render(renderer, "pg", SCREEN_WIDTH/2.0f - size/2.0f, SCREEN_HEIGHT - size, size);
+    }
+}
+
+int main() {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    TTF_Init();  // Initialize the SDL_ttf library
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);  // Initialize SDL_mixer
+
+    window = SDL_CreateWindow("DOOM", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Load the font
+    font = TTF_OpenFont("./assets/Arial Narrow.ttf", 24);  // Replace with the actual path to your font file and desired font size
+    if (!font) {
+        std::cout << "TTF_OpenFont error: " << TTF_GetError() << std::endl;
+    }
+
+    // Load the music file
+    Mix_Music* music = Mix_LoadMUS("./assets/tlouMusic.mp3");
+    if (!music) {
+        std::cout << "Mix_LoadMUS error: " << Mix_GetError() << std::endl;
+    }
+
+    // Load the sound effect file
+    Mix_Chunk* soundEffect = Mix_LoadWAV("./assets/tlouStep.mp3");
+    if (!soundEffect) {
+        // Handle sound effect loading error
+        // Print an error message or throw an exception
+    }
+
+    // Play the music in an infinite loop
+    if (Mix_PlayMusic(music, -1) == -1) {
+        // Handle music playing error
+        // Print an error message or throw an exception
+    }
+
+    // Load the images
+    ImageLoader::init();
+    ImageLoader::loadImage("+", "assets/wall3.jpeg");
+    ImageLoader::loadImage("-", "assets/wall1.jpeg");
+    ImageLoader::loadImage("|", "assets/wall2.jpeg");
+    ImageLoader::loadImage("*", "assets/wall4.jpeg");
+    ImageLoader::loadImage("g", "assets/window.jpg");
+    ImageLoader::loadImage("p", "assets/tlouUI.png");
+    ImageLoader::loadImage("pg", "assets/tlouUI2.png");
+
+    Raycaster r = { renderer };
+    r.load_map("assets/map.txt");
+
+    bool running = true;
+    int speed = 10;
+
+    int frameCount = 0;
+
+    displayWelcomeMessage();
+
+    // Mini event loop
+    Uint32 startTime = SDL_GetTicks();
+    Uint32 currentTime = startTime;
+    SDL_Event event;
+    while (SDL_GetTicks() - startTime < 2000) { // Run the loop for 3 seconds
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                SDL_Quit();
+                return 0;
+            }
+        }
+    }
+
+    int value = 0;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+                break;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_LEFT:
+                        r.player.a += 3.14 / 24;
+                        break;
+                    case SDLK_RIGHT:
+                        r.player.a -= 3.14 / 24;
+                        break;
+                    case SDLK_UP:
+                        if (value == 0){
+                            value = 1;
+                        } else {
+                            value = 0;
+                        }
+                        r.player.x += speed * cos(r.player.a);
+                        r.player.y += speed * sin(r.player.a);
+                        Mix_PlayChannel(-1, soundEffect, 0);
+                        break;
+                    case SDLK_DOWN:
+                        if (value == 0){
+                            value = 1;
+                        } else {
+                            value = 0;
+                        }
+                        r.player.x -= speed * cos(r.player.a);
+                        r.player.y -= speed * sin(r.player.a);
+                        Mix_PlayChannel(-1, soundEffect, 0);
+                        break;
+                    default:
+                        break;
+                }
+            } else if (event.type == SDL_MOUSEMOTION) {
+                r = handleMouseEvents(event, r);
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_RIGHT) {
+                    int res = r.render();
+                    
+                    if (res == 0) {
+                        displayWinningMessage();
+                        SDL_Delay(2000);
+                        running = false;
+                    }
+                }
+            }
+        }
+
+        clear();
+        draw_floor(value);
+
+        int res = r.render();
+
+        drawUI(value);
+
+        if (res == 1) {
+            displayLosingMessage();
+            SDL_Delay(2000);
+            running = false;
+        }
+
+        // render
+        SDL_RenderPresent(renderer);
+
+        frameCount++;
+        // Calculate and display FPS
+        if (SDL_GetTicks() - currentTime >= 1000) {
+            currentTime = SDL_GetTicks();
+            std::string title = "Doom - FPS: " + std::to_string(frameCount);
+            SDL_SetWindowTitle(window, title.c_str());
+            frameCount = 0;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ src/raycaster.h
@@ -0,0 +1,221 @@
+#pragma once
+
+#include <print.h>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <SDL_render.h>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <SDL2/SDL.h>
+#include <unordered_map>
+#include "color.h"
+#include "imageloader.h"
+
+
+const Color B = {0, 0, 0};
+const Color W = {255, 255, 255};
+
+const int WIDTH = 16;
+const int HEIGHT = 15;
+const int BLOCK = 50;
+const int SCREEN_WIDTH = WIDTH * BLOCK;
+const int SCREEN_HEIGHT = HEIGHT * BLOCK;
+
+
+struct Player {
+  int x;
+  int y;
+  float a;
+  float fov;
+}; 
+
+struct Impact {
+  float d;
+  std::string mapHit;  // + | -
+  int tx;
+};
+
+class Raycaster {
+public:
+  Raycaster(SDL_Renderer* renderer)
+    : renderer(renderer) {
+
+    player.x = BLOCK + BLOCK / 2;
+    player.y = BLOCK + BLOCK / 2;
+
+    player.a = M_PI / 4.0f;
+    player.fov = M_PI / 3.0f;
+
+    scale = 50;
+    tsize = 128;
+  }
+
+  void load_map(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    while (getline(file, line)) {
+      map.push_back(line);
+    }
+    file.close();
+  }
+
+  void point(int x, int y, Color c) {
+    SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+    SDL_RenderDrawPoint(renderer, x, y);
+  }
+
+  void rect(int x, int y, int blockSize, const std::string& mapHit) {
+    for(int cx = x; cx < x + blockSize; cx++) {
+      for(int cy = y; cy < y + blockSize; cy++) {
+        int tx = ((cx - x) * tsize) / blockSize;
+        int ty = ((cy - y) * tsize) / blockSize;
+
+        Color c = ImageLoader::getPixelColor(mapHit, tx, ty);
+        SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b , 255);
+        SDL_RenderDrawPoint(renderer, cx, cy);
+      }
+    }
+  }
+
+  Impact cast_ray(float a) {
+    float d = 0;
+    std::string mapHit;
+    int tx;
+
+    while(true) {
+      int x = static_cast<int>(player.x + d * cos(a)); 
+      int y = static_cast<int>(player.y + d * sin(a)); 
+      
+      int i = static_cast<int>(x / BLOCK);
+      int j = static_cast<int>(y / BLOCK);
+
+
+      if (map[j][i] != ' ') {
+        mapHit = map[j][i];
+
+        int hitx = x - i * BLOCK;
+        int hity = y - j * BLOCK;
+        int maxhit;
+
+        if (hitx == 0 || hitx == BLOCK - 1) {
+          maxhit = hity;
+        } else {
+          maxhit = hitx;
+        }
+
+        tx = maxhit * tsize / BLOCK;
+
+        break;
+      }
+     
+//      point(x, y, W);
+      
+      d += 1;
+    }
+    return Impact{d, mapHit, tx};
+  }
+
+  void draw_stake(int x, float h, Impact i) {
+    float start = SCREEN_HEIGHT/2.0f - h/2.0f;
+    float end = start + h;
+
+    for (int y = start; y < end; y++) {
+      int ty = (y - start) * tsize / h;
+      Color c = ImageLoader::getPixelColor(i.mapHit, i.tx, ty);
+      SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+
+      SDL_RenderDrawPoint(renderer, x, y);
+    }
+  }
+
+  int render() {
+
+    bool flag = false;
+
+    // Lanzar rayos y dibujar estacas en el lado izquierdo de la pantalla
+    for (int i = 1; i < SCREEN_WIDTH; i++) {
+        double a = player.a + player.fov / 2.0 - player.fov * i / SCREEN_WIDTH;
+        Impact impact = cast_ray(a);
+        float d = impact.d;
+        Color c = Color(255, 0, 0);
+
+        if (d == 0) {
+          return 1;
+        }
+
+        int x = i;
+        float h = static_cast<float>(SCREEN_HEIGHT) / static_cast<float>(d) * static_cast<float>(scale);
+        draw_stake(x, h, impact);
+
+        if (impact.mapHit == "g") {
+          flag = true;
+        }
+    }
+
+    // Dibujar el minimapa en la esquina superior derecha de la pantalla
+    int minimapSize = SCREEN_WIDTH / 7;  // Make the minimap smaller
+    int startX = SCREEN_WIDTH - minimapSize - 50;  // Coordenada x de inicio del minimapa, moved 10 pixels to the left
+    int startY = 0;  // Coordenada y de inicio del minimapa
+    int endX = SCREEN_WIDTH - 10;  // Coordenada x de fin del minimapa, moved 10 pixels to the left
+    int endY = minimapSize - 10;  // Coordenada y de fin del minimapa
+    // Adjust the size of the minimap and each block
+    int smallBlockSize = BLOCK / 5;  // Make each block smaller
+
+    // Set the draw color to black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    // Create an SDL_Rect for the minimap
+    SDL_Rect minimapRect;
+    minimapRect.x = startX;
+    minimapRect.y = startY;
+    minimapRect.w = minimapSize + 40;
+    minimapRect.h = minimapSize - 10;
+
+    // Fill the minimap area with the background color
+    SDL_RenderFillRect(renderer, &minimapRect);
+
+    for (int y = startY; y < endY; y += smallBlockSize) {  // Use smallBlockSize as the loop increment
+      for (int x = startX; x < endX; x += smallBlockSize) {  // Use smallBlockSize as the loop increment
+        int i = std::min(static_cast<int>((x - startX) / smallBlockSize), static_cast<int>(map[0].size() - 1));
+        int j = std::min(static_cast<int>((y - startY) / smallBlockSize), static_cast<int>(map.size() - 1));
+
+        int min = std::min(1, 10);
+
+        if (map[j][i] != ' ') {
+            std::string mapHit;
+            mapHit = map[j][i];
+            Color c = Color(255, 0, 0);
+            rect(x, y, smallBlockSize, mapHit);  // Pass smallBlockSize to the rect function
+        }
+      }
+    
+      if (flag) return 0;
+    }
+
+    // Dibujar el jugador en el minimapa
+    // Scale the player's coordinates to the minimap's size
+    int minimapPlayerX = startX + 10 + ((player.x * 1.8) / smallBlockSize);
+    int minimapPlayerY = startY + 10 + ((player.y * 1.8) / smallBlockSize);
+
+    // Ensure the point is within the bounds of the minimap
+    minimapPlayerX = std::max(startX, std::min(minimapPlayerX, endX - 1));
+    minimapPlayerY = std::max(startY, std::min(minimapPlayerY, endY - 1));
+
+    // Set the draw color to white
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    // Draw a point at the player's position on the minimap
+    SDL_RenderDrawPoint(renderer, minimapPlayerX, minimapPlayerY);
+
+    return 2;
+  }
+
+  Player player;
+private:
+  int scale;
+  SDL_Renderer* renderer;
+  std::vector<std::string> map;
+  int tsize;
+};
